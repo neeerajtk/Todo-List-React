9ebb7f68522df5894e79a592f5f8f7f38cee83cc
neeerajtk <neerajtk560@gmail.com>
2020-04-23 20:26:25 +0530

    form addded

---
+++ todo/src/NewTodoForm.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+class NewTodoForm extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            task:""       
+        }
+    this.handleChange=this.handleChange.bind(this);
+    }
+    handleChange(evt){
+        this.setState({
+            [evt.target.name]: evt.target.value
+        })
+    }
+
+    handleSubmit(evt){
+        evt.preventDefault();
+        
+    }
+    render(){
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <label htmlFor="task">New Todo</label>
+                <input 
+                type="text"
+                placeholder="new task"
+                id="task"
+                name="task"
+                value={this.state.task}
+                onChange={this.handleChange}
+                />
+                <button>Add Todo</button>
+            </form>
+    }
+}
+
+export default NewTodoForm;
