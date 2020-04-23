96a72f6856bc1c836e5834ec2da1ed15142b129e
neeerajtk <neerajtk560@gmail.com>
2020-04-23 22:44:51 +0530

    todo delete func added

--- todo/src/TodoList.js
+++ todo/src/TodoList.js
@@ -9,6 +9,12 @@ class TodoList extends Component{
             todos: []
         };
         this.create=this.create.bind(this);
+        this.remove=this.remove.bind(this);
+    }
+    remove(id){
+        this.setState({
+            todos: this.state.todos.filter(t => t.id !== id)
+        });
     }
 
     create(newTodo){
@@ -18,7 +24,7 @@ class TodoList extends Component{
     }
     render(){
         const todos = this.state.todos.map(todo=> {
-            return <Todo key={todo.id} task={todo.task} />
+            return <Todo key={todo.id} id={todo.id} task={todo.task} removeTodo={this.remove} />
         })
         return(
             <div>
