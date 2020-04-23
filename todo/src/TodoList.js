e6f3eef702e222ba5bc5ff95a7c09e3266e9d447
neeerajtk <neerajtk560@gmail.com>
2020-04-23 19:58:16 +0530

    mapping done

--- todo/src/TodoList.js
+++ todo/src/TodoList.js
@@ -1,19 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Todo from './Todo';
 
 class TodoList extends Component{
     constructor(props){
         super(props);
-        this.state = {todos: []}
+        this.state = {
+            todos: [{task: "Walk the fish"},{task: "Groom chickens"}]
+        };
     }
 
 
     render(){
+        const todos = this.state.todos.map(todo=> {
+            return <Todo task={todo.task} />
+        })
         return(
             <div>
                 <h1>Todo List !</h1>
                 <ul>
-                    <li>Todo2</li>
-                    <li>Todo2</li>
+                    {todos}
                 </ul>
             </div>
         )
